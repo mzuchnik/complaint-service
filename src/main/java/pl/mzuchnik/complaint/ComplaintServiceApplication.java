714776e03ee6b7c96bce6875777968306c181954
Mateusz Żuchnik <mateusz.zuchnik97@gmail.com>
2025-04-10 20:26:21 +0200

    Init commit

---
+++ src/main/java/pl/mzuchnik/complaint/ComplaintServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.mzuchnik.complaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComplaintServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mzuchnik/complaint/ComplaintServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mzuchnik.complaint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
